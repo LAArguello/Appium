7ab2db01c95146b6eee3a10e0f7310820d5b9d42
Leonel.Arguello <leonelarguellos52@gmail.com>
2022-05-24 09:59:57 -0300

    commit 2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
+import org.openqa.selenium.By;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import javax.print.DocFlavor.URL;
@@ -10,11 +11,11 @@ import javax.print.DocFlavor.URL;
 public class Main {
     static AppiumDriver<MobileElement> driverappium;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
        openApp();
 
     }
-    public static void openApp()  {
+    public static void openApp() throws InterruptedException {
 
         DesiredCapabilities cap= new DesiredCapabilities();
         cap.setCapability("deviceName", "S20 FE");
@@ -23,8 +24,18 @@ public class Main {
         cap.setCapability("platformVersion", "12.0.0");
         cap.setCapability("appPackage", "com.logictex.contactmanager");
         cap.setCapability("appActivity", "com.logictex.contactmanager.MainActivity");
-    //    URL url= new URL("http://127.0.0.1:4723/wd/hub");
+     // URL url= new URL("localhost/wd/hub");
         driverappium= new AppiumDriver<MobileElement>(cap);
+        MobileElement numeroTelefono= driverappium.findElement(By.id("com.logictex.contactmanager:id/TextPhone"));
+        MobileElement mail= driverappium.findElement(By.id("com.logictex.contactmanager:id/TextEmail"));
+        MobileElement direccion= driverappium.findElement(By.id("com.logictex.contactmanager:id/TextAddress"));
+        MobileElement nombreContacto= driverappium.findElement(By.id("com.logictex.contactmanager:id/TextName"));
+        MobileElement btnAgregar= driverappium.findElement(By.id("com.logictex.contactmanager:id/BtnAdd"));
+        nombreContacto.sendKeys("Marcos Orona");
+        mail.sendKeys("Markitosorona@gmail.com");
+        direccion.sendKeys("En un barrio de negros");
+        numeroTelefono.sendKeys("3804988783");
+        btnAgregar.click();
 
     }
 }
